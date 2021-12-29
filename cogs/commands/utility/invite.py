36588d4fc53bea905c345f60c384c7a67917f4bb
cy-bug <shashwat5590@gmail.com>
2021-12-29 20:45:31 +0530

    ELITE

--- cogs/commands/utility/invite.py
+++ cogs/commands/utility/invite.py
@@ -8,14 +8,13 @@ load_dotenv()
 
 class Invite(commands.Cog):
 	def __init__(self, client):
-		self.perms = os.getenv("permissions")
 		self.client = client
 
 	@commands.command(name='invite', aliases=['addbot'], help="Invite the bot to your server")
 	async def invite(self, ctx):
 		em = discord.Embed(
-			title="Thank you for inviting me",
-			description=f"Click [here](https://discord.com/api/oauth2/authorize?client_id={self.client.user.id}&permissions={self.perms}&scope=bot) to add me to your server\nClick [here](https://discord.gg/BgmX5V8tQW) to join the support server."
+			title="I'm glad you asked ```:D```",
+			description=f"\nClick [here](https://top.gg/bot/899263193568936028) to add me to your server\nClick [here](https://discord.gg/BgmX5V8tQW) to join the support server."
 		)
 		em.set_thumbnail(url=self.client.user.avatar_url)
 		em.set_footer(text='Bot created by <@663675391592103936>')
