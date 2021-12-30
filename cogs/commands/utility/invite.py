dbb6cee35af91867ffbaec45ee437081ed35c60c
cy-bug <shashwat5590@gmail.com>
2021-12-30 12:30:49 +0530

    added owner pfp to embed

--- cogs/commands/utility/invite.py
+++ cogs/commands/utility/invite.py
@@ -14,10 +14,14 @@ class Invite(commands.Cog):
 	async def invite(self, ctx):
 		em = discord.Embed(
 			title="I'm glad you asked ```:D```",
-			description=f"\nClick [here](https://top.gg/bot/899263193568936028) to add me to your server\nClick [here](https://discord.gg/BgmX5V8tQW) to join the support server."
+			description=f"\nClick [here](https://top.gg/bot/899263193568936028) to add me to your server\nClick ["
+			            f"here](https://discord.gg/BgmX5V8tQW) to join the support server. "
 		)
 		em.set_thumbnail(url=self.client.user.avatar_url)
-		em.set_footer(text='Bot created by <@663675391592103936>')
+		em.set_footer(
+			text='Bot created by <@663675391592103936>',
+			icon_url=self.client.get_user(self.client.owner_ids[0]).avatar_url
+		)
 
 		await ctx.send(embed=em)
 
