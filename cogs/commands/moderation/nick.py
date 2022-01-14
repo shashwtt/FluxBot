53886f9b356d9c7a9363eba7ddfae1c46d2bb4ab
cy-bug <shashwat5590@gmail.com>
2022-01-14 20:30:57 +0530

    POG

--- cogs/commands/moderation/nick.py
+++ cogs/commands/moderation/nick.py
@@ -11,23 +11,29 @@ class Nick(commands.Cog):
         self.client = client
 
     @commands.command(name='nick', aliases=['nickname', 'name'],
-                      help='Change someone\'s nickname on the server..')
-    @commands.has_permissions(manage_nicknames=True)
+                      help='Manage server nickname..')
     @commands.bot_has_permissions(manage_nicknames=True)
     async def nick(self, ctx, user: discord.Member, *, nickname):
+        if not ctx.author.has_permissions(manage_nicknames=True):
+            if ctx.author != user:
+                return
+
         if len(nickname) > 32:
             embed_err = discord.Embed(
                 description="A nickname cannot be longer than 32 digits...\n\nChoose a shorter nickname!",
-                colour=discord.Colour.brand_red()
+                colour=discord.Colour.yellow()
             )
+            await ctx.send(embed=embed_err)
             return
 
-        try:
-            await user.edit(nick=nickname)
-        except Exception:
-            print(Exception)
-
-
+        new_user = await user.edit(nick=nickname)
+        embed = discord.Embed(
+            description=f"{user.mention}'s nickname was changed by {ctx.author.mention}!",
+            colour=discord.Colour.brand_green()
+        )
+        embed.add_field(name="nickname before -", value=user.nick)
+        embed.add_field(name="nickname now -", value=new_user.nick)
+        await ctx.send()
 
 def setup(client):
     client.add_cog(Nick(client))
