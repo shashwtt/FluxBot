827c6c74f858bed29fa592b041d35d14d90a30e5
cy-bug <shashwat5590@gmail.com>
2022-01-14 10:53:24 +0530

    POG

--- cogs/commands/memes/meme.py
+++ cogs/commands/memes/meme.py
@@ -25,36 +25,68 @@ class Meme(Cog):
     def __init__(self, client):
         self.client = client
 
-    @commands.command(
-        name='meme',
-        aliases=['maymay', 'm'],
-        help="Brings a post from meme subreddits..")
+    async def edit_meme_new(self, ctx, interaction):
+        async def next_meme_callback(msg_inter):
+            await self.edit_meme_new(ctx, msg_inter)
+
+        async def close_meme_view(msg_inter=None):
+            meme_view.stop()
+            meme_view.clear_items()
+            if msg_inter is None:
+                await interaction.message.edit(view=None)
+            else:
+                await msg_inter.message.edit(view=None)
+
+        meme = get_meme()
+        post = meme[0]
+        image = meme[1]
+        title = meme[2]
+
+        meme_view = discord.ui.View(timeout=15)
+        next_meme = discord.ui.Button(label="Next meme!", style=discord.ButtonStyle.green)
+        next_meme.callback = next_meme_callback
+        close_view = discord.ui.Button(emoji="<:x_white:930381127535984641>", style=discord.ButtonStyle.danger)
+        close_view.callback = close_meme_view
+        meme_view.add_item(next_meme)
+
+        ctx.message.emed.title = title
+        ctx.message.embed.set_imgage(url=image)
+        ctx.message.embed.set_footer(text=f"👍 {post['ups']} | Posted by - [u/{post['author']}](https://www.reddit.com/u/{post['author']})")
+
+        interaction.message.edit(embed=ctx.message.embed, view=meme_view)
+
+    @commands.command(name='meme', aliases=['maymay', 'm'], help="Brings a post from meme subreddits..")
     @cooldown(1, 3, BucketType.user)
     async def meme(self, ctx):
-
         async def next_meme_callback(interaction):
-            x = await send_meme()
-            await interaction.message.edit(embed=x[0], view=x[1])
+            await self.edit_meme_new(ctx, interaction)
 
-        async def send_meme():
-            meme = get_meme()
-            post = meme[0]
-            image = meme[1]
-            title = meme[2]
-            meme_view = discord.ui.View(timeout=15)
-            next_meme = discord.ui.Button(label="Next meme!", style=discord.ButtonStyle.green)
-            next_meme.callback = next_meme_callback
-            meme_view.add_item(next_meme)
+        async def close_meme_view(interaction=None):
+            meme_view.stop()
+            meme_view.clear_items()
+            if interaction is None:
+                message.edit(view=None)
+            else:
+                await interaction.message.edit(view=None)
 
-            em = discord.Embed(
-                title=title,
-                color=random.choice(hex_colors.colors))
-            em.set_image(url=image)
-            em.set_footer(text=f"👍 {post['ups']} | Author: u/{post['author']}")
-            return [em, meme_view]
+        meme = get_meme()
+        post = meme[0]
+        image = meme[1]
+        title = meme[2]
+        meme_view = discord.ui.View(timeout=15)
+        next_meme = discord.ui.Button(label="Next meme!", style=discord.ButtonStyle.green)
+        next_meme.callback = next_meme_callback
+        close_view = discord.ui.Button(emoji="<:x_white:930381127535984641>", style=discord.ButtonStyle.danger)
+        close_view.callback = close_meme_view
+        meme_view.add_item(next_meme)
 
-        x = await send_meme()
-        await ctx.send(embed=x[0], view=x[1])
+        em = discord.Embed(
+            title=title,
+            color=random.choice(hex_colors.colors))
+        em.set_image(url=image)
+        em.set_footer(text=f"👍 {post['ups']} | Author: u/{post['author']}")
+
+        message = await ctx.send(embed=em, view=meme_view)
 
 
 def setup(client):
