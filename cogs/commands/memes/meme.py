b4bdf60ca6b3cab95878962d88155e7249577cf3
cy-bug <shashwat5590@gmail.com>
2022-01-14 20:05:45 +0530

    POG

--- cogs/commands/memes/meme.py
+++ cogs/commands/memes/meme.py
@@ -25,53 +25,27 @@ class Meme(Cog):
 	def __init__(self, client):
 		self.client = client
 
-	async def edit_meme_new(self, ctx, interaction):
-		async def next_meme_callback(msg_inter):
-			await self.edit_meme_new(ctx, msg_inter)
-
-		async def close_meme_view(msg_inter=None):
-			meme_view.stop()
-			meme_view.clear_items()
-			if msg_inter is None:
-				await interaction.message.edit(view=None)
-			else:
-				await msg_inter.message.edit(view=None)
-
-		meme = get_meme()
-		post = meme[0]
-		image = meme[1]
-		title = meme[2]
-
-		meme_view = discord.ui.View(timeout=15)
-		next_meme = discord.ui.Button(label="Next meme!", style=discord.ButtonStyle.green)
-		next_meme.callback = next_meme_callback
-		close_view = discord.ui.Button(emoji="<:x_white:930381127535984641>", style=discord.ButtonStyle.danger)
-		close_view.callback = close_meme_view
-		meme_view.add_item(next_meme)
-		meme_view.add_item(close_view)
-		meme_view.on_timeout = close_meme_view
-
-		em = discord.Embed(
-			title=title,
-			color=random.choice(hex_colors.colors))
-		em.set_image(url=image)
-		em.set_footer(text=f"👍 {post['ups']} | Author: u/{post['author']}")
-
-		await interaction.message.edit(embed=em, view=meme_view)
-
 	@commands.command(name='meme', aliases=['maymay', 'm'], help="Brings a post from meme subreddits..")
 	@cooldown(1, 3, BucketType.user)
 	async def meme(self, ctx):
 		async def next_meme_callback(interaction):
-			await self.edit_meme_new(ctx, interaction)
+			my_meme = get_meme()
+			may = my_meme[0]
+			memage = my_meme[1]
+			mimle = my_meme[2]
+			membed = interaction.message.embeds[0]
+			membed.set_image(url=memage)
+			membed.title = mimle
+			membed.set_footer(text=f"👍 {may['ups']} | Author: u/{may['author']}")
+			membed.color = random.choice(hex_colors.colors)
 
-		async def close_meme_view(interaction=None):
-			meme_view.stop()
-			meme_view.clear_items()
-			if interaction is None:
-				await message.edit(view=None)
-			else:
-				await interaction.message.edit(view=None)
+			await interaction.message.edit(embed=membed)
+
+		async def close_meme_view(interaction):
+			await interaction.message.edit(view=None)
+
+		async def timeout_view():
+			await message.edit(view=None)
 
 		meme = get_meme()
 		post = meme[0]
@@ -84,7 +58,7 @@ class Meme(Cog):
 		close_view.callback = close_meme_view
 		meme_view.add_item(next_meme)
 		meme_view.add_item(close_view)
-		meme_view.on_timeout = close_meme_view
+		meme_view.on_timeout = timeout_view
 
 		em = discord.Embed(
 			title=title,
