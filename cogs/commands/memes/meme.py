bf41a0b7908c00473e1fb66c932ac3e30449f691
cy-bug <shashwat5590@gmail.com>
2022-01-14 09:07:12 +0530

    POG

--- cogs/commands/memes/meme.py
+++ cogs/commands/memes/meme.py
@@ -9,6 +9,18 @@ from discord.ext import commands
 from discord.ext.commands import cooldown, BucketType, Cog
 
 
+def get_meme():
+    subreddits = ['memes', 'dankmemes']
+    subreddit = random.choice(subreddits)
+    url = f'https://meme-api.herokuapp.com/gimme/{subreddit}'
+    response = requests.get(url)
+    post = response.json()
+    image = post['url']  # the meme (a reddit post)
+    title = post['title']  # the title of the reddit post
+
+    return [post, image, title]
+
+
 class Meme(Cog):
     def __init__(self, client):
         self.client = client
@@ -19,21 +31,30 @@ class Meme(Cog):
         help="Brings a post from meme subreddits..")
     @cooldown(1, 3, BucketType.user)
     async def meme(self, ctx):
-        subreddits = ['memes', 'dankmemes']
-        subreddit = random.choice(subreddits)
-        url = f'https://meme-api.herokuapp.com/gimme/{subreddit}'
-        response = requests.get(url)
-        post = response.json()
-        image = post['url']  # the meme (a reddit post)
-        title = post['title']  # the title of the reddit post
 
-        em = discord.Embed(
-            title=title,
-            color=random.choice(hex_colors.colors))
-        em.set_image(url=image)
-        em.set_footer(text=f"👍 {post['ups']} | Author: u/{post['author']}")
+        async def next_meme_callback(interaction):
+            x = await send_meme()
+            interaction.message.edit(embed=x[0], view=x[1])
 
-        await ctx.send(embed=em)
+        async def send_meme():
+            meme = get_meme()
+            post = meme[0]
+            image = meme[1]
+            title = meme[2]
+            meme_view = discord.ui.View(timeout=15)
+            next_meme = discord.ui.Button(label="Next meme!", style=discord.ButtonStyle.green)
+            next_meme.callback = next_meme_callback
+            meme_view.add_item(next_meme)
+
+            em = discord.Embed(
+                title=title,
+                color=random.choice(hex_colors.colors))
+            em.set_image(url=image)
+            em.set_footer(text=f"👍 {post['ups']} | Author: u/{post['author']}")
+            return [em, meme_view]
+
+        x = await send_meme()
+        ctx.send(embed=x[0], view=x[1])
 
 
 def setup(client):
--- cogs/commands/utility/ping.py
+++ cogs/commands/utility/ping.py
@@ -21,7 +21,7 @@ class Ping(Cog):
 			color=0x42F56C
 		)
 		custom_view = View()
-		custom_url = 'https://discord.gg/meCMJRYGvp'
+		custom_url = 'https://discordapp.com/channels/@me/663675391592103936'
 		custom_label = 'Report a problem!'
 		custom_button = Button(label=custom_label, url=custom_url)
 		custom_view.add_item(custom_button)
